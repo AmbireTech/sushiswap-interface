2271b1a0896f7d7c0d378520b84ed0db540f06be
IvoPaunov <ivo.paunov@gmail.com>
2022-03-22 11:04:36 +0200

    feat: update styles

--- src/layouts/SwapLayout.tsx
+++ src/layouts/SwapLayout.tsx
@@ -9,7 +9,7 @@ export interface Layout {
 }
 
 export const SwapLayoutCard: FC = ({ children }) => {
-  return <div className="flex flex-col gap-3 p-2 pt-4 shadow-md md:p-4 bg-dark-800 shadow-dark-1000">{children}</div>
+  return <div className="flex flex-col gap-3 p-2 pt-4 md:p-4 bg-dark-800">{children}</div>
 }
 
 export const Layout: FC<Layout> = ({ children, id }) => {
